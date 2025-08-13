9b72cc38782e8c4d5da387caf48ca513cec835fa
Aaron Sanchez <saarong12@gmail.com>
2025-08-12 23:58:40 -0500

    Initialized a comp library with vite.
    Also created a starter component for testing.

---
+++ lib/Accordion/Accordion.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Accordion.scss'
+
+export const Accordion: React.FC = ({}) => {
+  return (
+    <div>
+      <p>Accordion goes here.</p>
+    </div>
+  )
+}
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      name: 'CompUiProto',
+      fileName: 'comp-ui-proto'
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  }
+})
